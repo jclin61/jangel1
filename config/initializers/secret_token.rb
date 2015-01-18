ddd4c54720e0213e53b1f9486d90786b23779d3b
Jerry C. Lin <jerryclin@hotmail.com>
2015-01-17 21:01:25 -0800

    initial git setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jangel1::Application.config.secret_key_base = '4eed00172394cb908ca5dc7d09905665460e164ea66912a9a93d7a38a7e3aa1611b527e263fdd94f278e53fd63b7d9c135994c8c63b5ecb78671a966d1c21b7d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jangel1::Application.config.session_store :cookie_store, key: '_jangel1_session'
