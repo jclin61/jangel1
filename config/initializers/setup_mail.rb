185f1bc3ce7dbea877722ba0523c8e39879778d6
Jerry C. Lin <jerryclin@hotmail.com>
2015-03-26 02:20:22 -0700

    removed parantheses for ENV value in smtp mailer setting

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'ENV['GMAIL_ID']',
-  password:             'ENV['GMAIL_PASSWORD']',
+  user_name:            ENV['GMAIL_ID'],
+  password:             ENV['GMAIL_PASSWORD'],
   authentication:       'plain',
   enable_starttls_auto: true 
 }
